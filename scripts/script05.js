c20b6fa9edd6de482eddd2494be49b4cc2ca9d7f
Michael Crump <mbcrump29@gmail.com>
2014-08-31 13:04:14 -0500

    update

---
+++ scripts/script05.js
@@ -0,0 +1,25 @@
+(function(angular) {
+
+
+    function MainController($scope, $http) {
+        $scope.message = "Github User Lookup";
+
+        var onUserComplete = function(response){
+            $scope.user = response.data;
+        };
+
+        var onError = function(reason){
+            $scope.error = "Could not reach the server";
+        };
+
+        $scope.search = function(username){
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError);
+        };
+
+
+    }
+
+    angular.module("app", []).controller("MainController", ["$scope", "$http", MainController]);
+
+})(angular);
