827d4a0c481c8892ee0cc0dd04c3fe87bcb5f871
Michael Crump <mbcrump29@gmail.com>
2014-09-07 10:19:44 -0500

    Added more Angular

---
+++ scripts/script09.js
@@ -0,0 +1,49 @@
+(function(angular) {
+
+
+    function MainController($scope, $http, $interval) {
+
+
+        var onUserComplete = function(response){
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
+
+        var onRepos = function(response){
+          $scope.repos = response.data;
+        };
+
+        var onError = function(reason){
+            $scope.error = "Could not reach the server";
+        };
+
+        var decrementCountdown = function(){
+            $scope.countdown -= 1;
+            if($scope.countdown < 1){
+                $scope.search($scope.username);
+            };
+
+        };
+
+        var startCountdown = function () {
+            $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
+        $scope.search = function(username){
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError);
+        };
+
+        $scope.message = "Github User Lookup";
+        $scope.username = "mbcrump";
+        $scope.repoSortOrder = "stargazers_count"
+        $scope.countdown = 5;
+        startCountdown();
+
+
+    }
+
+    angular.module("app", []).controller("MainController", ["$scope", "$http", "$interval", MainController]);
+
+})(angular);
