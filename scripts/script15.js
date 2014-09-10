f454e7a41d035fd8afb2de9c8e56e2360ebf0021
Michael Crump <mbcrump29@gmail.com>
2014-09-10 17:24:41 -0500

    Added more angular

--- scripts/script15.js
+++ scripts/script15.js
@@ -5,12 +5,16 @@
     app.config(function($routeProvider){
         $routeProvider
             .when("/main", {
-                templateUrl: "main14.html",
+                templateUrl: "main15.html",
                 controller: "MainController"
             })
             .when("/user/:username", {
-                templateUrl: "user14.html",
+                templateUrl: "user15.html",
                 controller: "UserController"
+            })
+            .when("/repo/:username/:reponame", {
+                templateUrl: "repo15.html",
+                controller: "RepoController"
             })
             .otherwise({redirectTo:"/main"});
     });
